7f87c90a5c6d85d392a7734aa7ab2392e1040450
Kyle Miranda <kylemiranda0415@gmail.com>
2024-12-05 00:22:46 -0500

    Hope this works

--- express/server.js
+++ express/server.js
@@ -20,10 +20,7 @@ app.use(cookieParser());
 app.use(express.json()); // Middleware to parse JSON bodies
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
